89c0eda144589c460ab6f5c190264fdd2604c2eb
lucas <1783022886@qq.com>
2024-04-27 16:23:29 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jasonlabz/hive
+
+go 1.21
+
+require github.com/apache/thrift v0.20.0
---
+++ test/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//	conn, err := gohive.Connect("127.0.0.1:10000", gohive.DefaultOptions) // 无用户名、密码
+	conn, err := gohive.ConnectWithUser("127.0.0.1:10000", "username", "password", gohive.DefaultOptions) // 需要用户名、密码
+	if err != nil {
+		fmt.Errorf("Connect error %v", err)
+	}
+
+	_, err = conn.Exec("create table if not exists t(c1 int)")
+	_, err = conn.Exec(" insert into default.t values(1), (2)")
+	if err != nil {
+		fmt.Errorf("Connection.Exec error: %v", err)
+	}
+	rs, err := conn.Query("select c1 from t limit 10")
+	if err != nil {
+		fmt.Errorf("Connection.Query error: %v", err)
+	}
+	var c1 int
+	for rs.Next() {
+		rs.Scan(&c1)
+		fmt.Println(c1)
+	}
+	conn.Close()
+}
